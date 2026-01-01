dd4884d40a6f4ab552f29475d87e530740f20fec
wave745 <shazarno1@gmail.com>
2026-01-01 22:03:15 +0100

    init

--- lib/db.ts
+++
@@ -1,7 +0,0 @@
-const { PrismaClient } = require('../../node_modules/.prisma/client/client')
-
-const globalForPrisma = global as unknown as { prisma: any }
-
-export const prisma = globalForPrisma.prisma || new PrismaClient()
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
