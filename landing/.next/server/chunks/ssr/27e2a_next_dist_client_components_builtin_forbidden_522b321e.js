bb562e2991f7fbaa69d621bdb3ff3b1b134dd634
wave745 <shazarno1@gmail.com>
2026-01-02 15:55:02 +0100

    Remove build artifacts from git tracking

--- landing/.next/server/chunks/ssr/27e2a_next_dist_client_components_builtin_forbidden_522b321e.js
+++
@@ -1,3 +0,0 @@
-module.exports=[26136,(a,b,c)=>{"use strict";Object.defineProperty(c,"__esModule",{value:!0}),Object.defineProperty(c,"default",{enumerable:!0,get:function(){return f}});let d=a.r(33134),e=a.r(40535);function f(){return(0,d.jsx)(e.HTTPAccessErrorFallback,{status:403,message:"This page could not be accessed."})}("function"==typeof c.default||"object"==typeof c.default&&null!==c.default)&&void 0===c.default.__esModule&&(Object.defineProperty(c.default,"__esModule",{value:!0}),Object.assign(c.default,c),b.exports=c.default)}];
-
-//# sourceMappingURL=27e2a_next_dist_client_components_builtin_forbidden_522b321e.js.map
